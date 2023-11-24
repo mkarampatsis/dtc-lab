1746dd33800d149bbae6c2137962ca6417aa11d3
mkarampatsis <marka@central.ntua.gr>
2023-11-24 23:50:29 +0200

    changes to design

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbNavModule],
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent {
-
+  active = 1;
 }
--- src/app/components/lab/lab.component.ts
+++ src/app/components/lab/lab.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-lab',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbNavModule],
   templateUrl: './lab.component.html',
   styleUrls: ['./lab.component.css']
 })
 export class LabComponent {
-
+  active = 1;
 }
