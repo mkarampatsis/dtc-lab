206fd580f86c60384f6aed49797839125a21b263
mkarampatsis <marka@central.ntua.gr>
2024-01-05 00:35:27 +0200

    lab

--- src/app/components/lab/regulation/regulation.component.ts
+++ src/app/components/lab/regulation/regulation.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-regulation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './regulation.component.html',
   styleUrl: './regulation.component.css'
 })
--- src/app/components/lab/staff/staff.component.ts
+++ src/app/components/lab/staff/staff.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-staff',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbAccordionModule],
   templateUrl: './staff.component.html',
   styleUrl: './staff.component.css'
 })
